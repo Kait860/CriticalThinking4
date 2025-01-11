7910d12a284e1aca644922c9f407a74d445e2f12
Kait860 <schneiderkaitlyn04@gmail.com>
2025-01-11 00:38:54 -0600

    Add files via upload

--- Shape.java
+++ Shape.java
@@ -100,6 +100,19 @@ class Triangle extends Shape{
 
 class ShapeArray {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //initialize shape values
+        Rectangle rect = new Rectangle(5.0,8.0);
+        Circle circ = new Circle(3.0);
+        Triangle tri = new Triangle(3.4, 2.7, 5.2);
+
+        Shape[] ShapeArray = new Shape[3];
+        ShapeArray[0] = rect;
+        ShapeArray[1] = circ;
+        ShapeArray[2] = tri;
+
+        for(int i = 0; i < ShapeArray.length; i++){
+            ShapeArray[i].toString();
+        }
+
     }
 }
